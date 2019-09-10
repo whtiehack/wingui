8b51dedd53c19511d38937516d06bad216f73dfc
Hack White <whitehack@163.com>
2019-09-10 15:17:52 +0800

    examples:init wowjump

---
+++ examples/wowjump/main.go
@@ -0,0 +1,7 @@
+package main
+
+// optional  genereate resource IDs
+//go:generate go run github.com/whtiehack/wingui/tools/genids -filename ui/resource.h -packagename main
+func main() {
+
+}
---
+++ examples/wowjump/ui/resource.h
@@ -0,0 +1,11 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG_MAIN                         101
+#define IDE_INPUT_TIME                          40000
+#define IDE_LOG                                 40001
+#define IDB_RUN                                 40002
+#define IDE_NORMAL_TIME                         40003
+#define IDE_CHAR_WAIT_TIME                      40004
+#define IDE_ENTER_TIME                          40005
