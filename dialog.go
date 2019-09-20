a2433d63a3cb2d35c351339a2fe71b470771f877
smallwhite <whitehack@163.com>
2019-09-20 10:25:14 +0800

    TODO, WM_DELETEITEM

--- dialog.go
+++ dialog.go
@@ -75,16 +75,6 @@ func NewModalDialog(idd uintptr, parent win.HWND, dialogConfig *DialogConfig, cb
 func (dlg *Dialog) dialogWndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 	//log.Println("NewDialog.WndProc", hwnd, "msg:", msg, "wparam:", strconv.FormatInt(int64(win.HIWORD(uint32(wParam))), 16), win.LOWORD(uint32(wParam)), "lparam:", lParam)
 	// process subclassing items
-	if dlg.hwnd != hwnd {
-		if item, ok := dlg.items[hwnd]; ok {
-			return item.WndProc(msg, wParam, lParam)
-		}
-	}
-	if lParam != 0 && dlg.hwnd == hwnd {
-		if item, ok := dlg.items[win.HWND(lParam)]; ok {
-			return item.WndProc(msg, wParam, lParam)
-		}
-	}
 	switch msg {
 	//case win.WM_ACTIVATEAPP:
 	//
@@ -101,9 +91,9 @@ func (dlg *Dialog) dialogWndProc(hwnd win.HWND, msg uint32, wParam, lParam uintp
 			return 0
 		}
 		return 1
-	case win.WM_COMMAND:
-		// process WM_COMMAND for dlg items.
-		return 0
+	//case win.WM_COMMAND:
+	//	// process WM_COMMAND for dlg items.
+	//	return 0
 	case win.WM_CLOSE:
 		log.Println("WM_CLOSE", hwnd)
 		if dlg.cb != nil {
@@ -119,8 +109,34 @@ func (dlg *Dialog) dialogWndProc(hwnd win.HWND, msg uint32, wParam, lParam uintp
 			win.PostQuitMessage(0)
 		}
 		return 0
-	}
+	case win.WM_DELETEITEM:
+		// TODO
+		// Sent to the owner of a list box or combo box when the list box or combo box is destroyed or
+		// when items are removed by the LB_DELETESTRING, LB_RESETCONTENT, CB_DELETESTRING, or CB_RESETCONTENT message.
+		// The system sends a WM_DELETEITEM message for each deleted item. The system sends the WM_DELETEITEM message
+		// for any deleted list box or combo box item with nonzero item data.
+		// Parameters
+		//wParam
+		//Specifies the identifier of the control that sent the WM_DELETEITEM message.
+		//
+		//lParam
+		//Pointer to a DELETEITEMSTRUCT structure that contains information about the item deleted from a list box.
+		// https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-deleteitemstruct
+		//
+		//Return value
+		//An application should return TRUE if it processes this message.
 
+	}
+	if dlg.hwnd != hwnd {
+		if item, ok := dlg.items[hwnd]; ok {
+			return item.WndProc(msg, wParam, lParam)
+		}
+	}
+	if lParam != 0 && dlg.hwnd == hwnd {
+		if item, ok := dlg.items[win.HWND(lParam)]; ok {
+			return item.WndProc(msg, wParam, lParam)
+		}
+	}
 	return uintptr(0)
 }
 
