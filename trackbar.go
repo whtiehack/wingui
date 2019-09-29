dc395b5f10d3663084325f4c62622df8d8b0cd52
smallwhite <whitehack@163.com>
2019-09-29 18:29:20 +0800

    update TrackBar.

--- trackbar.go
+++ trackbar.go
@@ -51,6 +51,26 @@ func (tb *TrackBar) GetBuddy(isLeftOrTop bool) win.HWND {
 	return win.HWND(tb.SendMessage(winapi.TBM_GETBUDDY, uintptr(rd), 0))
 }
 
+//	SetBuddy assigns a window as the buddy window for a trackbar control.
+//	Trackbar buddy windows are automatically displayed in a location relative to the control's
+//	orientation (horizontal or vertical).
+//	isLeftOrTop indicating which buddy window handle will be retrieved, by relative location.
+//	This value can be one of the following:
+//	- TRUE
+//	- Retrieves the handle to the buddy to the left of the trackbar. If the trackbar control uses the TBS_VERT style, the message will retrieve the buddy above the trackbar.
+//	- FALSE
+//	- Retrieves the handle to the buddy to the right of the trackbar. If the trackbar control uses the TBS_VERT style, the message will retrieve the buddy below the trackbar.
+//
+//	hWnd is a Handle to the window that will be set as the trackbar control's buddy.
+//	Returns the handle to the window that was previously assigned to the control at that location.
+func (tb *TrackBar) SetBuddy(isLeftOrTop bool, hWnd win.HWND) win.HWND {
+	rd := 0
+	if isLeftOrTop {
+		rd = 1
+	}
+	return win.HWND(tb.SendMessage(winapi.TBM_SETBUDDY, uintptr(rd), uintptr(hWnd)))
+}
+
 //	Retrieves the size and position of the bounding rectangle for a trackbar's channel.
 //	(The channel is the area over which the slider moves. It contains the highlight when a range is selected.)
 //	Return value is a RECT structure. This structure with the channel's bounding rectangle,
@@ -72,6 +92,21 @@ func (tb *TrackBar) GetLineSize() int {
 	return int(tb.SendMessage(winapi.TBM_GETLINESIZE, 0, 0))
 }
 
+//	SetLineSize sets the number of logical positions the trackbar's slider moves in response to keyboard
+//	input from the arrow keys, such as the or keys. The logical positions are the integer increments in the
+//	trackbar's range of minimum to maximum slider positions.
+//	size is new line size.
+//	Returns a 32-bit value that specifies the previous line size.
+//
+//	Remarks
+//	The default setting for the line size is 1.
+//
+//	The trackbar also sends a WM_HSCROLL or WM_VSCROLL message with the TB_LINEUP and TB_LINEDOWN notification codes
+//	to its parent window when the user presses the arrow keys.
+func (tb *TrackBar) SetLineSize(size int) int {
+	return int(tb.SendMessage(winapi.TBM_SETLINESIZE, 0, uintptr(size)))
+}
+
 //	GetNumTics retrieves the number of tick marks in a trackbar.
 //	If no tick flag is set, it returns 2 for the beginning and ending ticks.
 //	If TBS_NOTICKS is set, it returns zero. Otherwise,
@@ -81,6 +116,38 @@ func (tb *TrackBar) GetLineSize() int {
 //	including the first and last tick marks created by the trackbar.
 func (tb *TrackBar) GetNumTics() int {
 	return int(tb.SendMessage(winapi.TBM_GETNUMTICS, 0, 0))
+}
+
+//	GetTic retrieves the logical position of a tick mark in a trackbar. The logical position can be any of the integer
+//	values in the trackbar's range of minimum to maximum slider positions.
+//	idx is a zero-based index identifying a tick mark. Valid indexes are in the range from zero to two less
+//	than the tick count returned by the TBM_GETNUMTICS message.
+//	Returns the logical position of the specified tick mark, or -1 if wParam does not specify a valid index.
+func (tb *TrackBar) GetTic(idx int) int {
+	return int(tb.SendMessage(winapi.TBM_GETTIC, uintptr(idx), 0))
+}
+
+//	GetTicPos retrieves the current physical position of a tick mark in a trackbar.
+//	idx is a zero-based index identifying a tick mark. The positions of the first and last tick marks are not
+//	directly available via this message.
+//
+//	Returns the distance, in client coordinates, from the left or top of the trackbar's client area to
+//	the specified tick mark. The return value is the x-coordinate of the tick mark for a horizontal trackbar or
+//	the y-coordinate for a vertical trackbar. If wParam is not a valid index, the return value is -1.
+//
+//	Remarks
+//	Because the first and last tick marks are not available through this message, valid indexes are offset from
+//	their tick position on the trackbar. If the difference between TBM_GETRANGEMIN and TBM_GETRANGEMAX is less than two,
+//	then there is no valid index and this message will fail.
+//
+//	The following illustrates the relation between the ticks on a trackbar, the ticks available through this message,
+//	and their zero-based indexes.
+//
+//	0 1 2 3 4 5 6 7 8 9    // Tick positions seen on the trackbar.
+//    1 2 3 4 5 6 7 8      // Tick positions whose position can be identified.
+//    0 1 2 3 4 5 6 7      // Index numbers for the identifiable positions.
+func (tb *TrackBar) GetTicPos(idx int) int {
+	return int(tb.SendMessage(winapi.TBM_GETTICPOS, uintptr(idx), 0))
 }
 
 //	GetPageSize retrieves the number of logical positions the trackbar's slider moves in response to keyboard input,
@@ -95,11 +162,48 @@ func (tb *TrackBar) GetPageSize() int {
 	return int(tb.SendMessage(winapi.TBM_GETPAGESIZE, 0, 0))
 }
 
+//	SetPageSize sets the number of logical positions the trackbar's slider moves in response to keyboard input,
+//	such as the or keys, or mouse input, such as clicks in the trackbar's channel. The logical positions are the
+//	integer increments in the trackbar's range of minimum to maximum slider positions.
+//	Returns a 32-bit value that specifies the previous page size.
+//
+//	Remarks
+//	The trackbar also sends a WM_HSCROLL or WM_VSCROLL message with the TB_PAGEUP and TB_PAGEDOWN notification codes to
+//	its parent window when it receives keyboard or mouse input that scrolls the page.
+func (tb *TrackBar) SetPageSize(newSize int) int {
+	return int(tb.SendMessage(winapi.TBM_SETPAGESIZE, 0, uintptr(newSize)))
+}
+
 //	GetPos retrieves the current logical position of the slider in a trackbar.
 //	The logical positions are the integer values in the trackbar's range of minimum to maximum slider positions.
 //	Returns a 32-bit value that specifies the current logical position of the trackbar's slider.
 func (tb *TrackBar) GetPos() int {
 	return int(tb.SendMessage(winapi.TBM_GETPOS, 0, 0))
+}
+
+//	SetPos sets the current logical position of the slider in a trackbar.
+//
+//	newPose is new logical position of the slider. Valid logical positions are the integer values
+//	in the trackbar's range of minimum to maximum slider positions. If this value is outside the control's
+//	maximum and minimum range, the position is set to the maximum or minimum value.
+func (tb *TrackBar) SetPos(redraw bool, newPos int) {
+	rd := 0
+	if redraw {
+		rd = 1
+	}
+	tb.SendMessage(winapi.TBM_SETPOS, uintptr(rd), uintptr(newPos))
+}
+
+//	SetPosNotify sets the current logical position of the slider in a trackbar.
+//	newPost New logical position of the slider. Valid logical positions are the integer values in the trackbar's
+//	range of minimum to maximum slider positions. If this value is outside the control's maximum and minimum range,
+//	the position is set to the maximum or minimum value.
+//
+//	Remarks
+//	Calling TBM_SETPOSNOTIFY will set the trackbar slider location like TBM_SETPOS would,
+//	but it will also cause the trackbar to notify its parent of a move via a WM_HSCROLL or WM_VSCROLL message.
+func (tb *TrackBar) SetPosNotify(newPos int) {
+	tb.SendMessage(winapi.TBM_SETPOSNOTIFY, 0, uintptr(newPos))
 }
 
 // TODO: TBM_GETPTICS
@@ -116,6 +220,94 @@ func (tb *TrackBar) GetRangeMax() int {
 //	maximum slider positions
 func (tb *TrackBar) GetRangeMin() int {
 	return int(tb.SendMessage(winapi.TBM_GETRANGEMIN, 0, 0))
+}
+
+//	SetRange sets the range of minimum and maximum logical positions for the slider in a trackbar.
+//
+//	Remarks
+//	If the current slider position is outside the new range,
+//	the TBM_SETRANGE message sets the slider position to the new maximum or minimum value.
+//
+//	Because this message takes two 16-bit unsigned integer values,
+//	the maximum range that this message can specify is from 0 to 65,535.
+//	To specify larger range values, use the TBM_SETRANGEMIN and TBM_SETRANGEMAX messages.
+func (tb *TrackBar) SetRange(redraw bool, min, max uint16) {
+	rd := 0
+	if redraw {
+		rd = 1
+	}
+	tb.SendMessage(winapi.TBM_SETRANGE, uintptr(rd), uintptr(win.MAKELONG(min, max)))
+}
+
+//	SetRangeMax Sets the maximum logical position for the slider in a trackbar.
+//	max Maximum position for the slider.
+//
+//	Remarks
+//	If the current slider position is greater than the new maximum,
+//	the TBM_SETRANGEMAX message sets the slider position to the new maximum value.
+func (tb *TrackBar) SetRangeMax(redraw bool, max int) {
+	rd := 0
+	if redraw {
+		rd = 1
+	}
+	tb.SendMessage(winapi.TBM_SETRANGEMAX, uintptr(rd), uintptr(max))
+}
+
+//	SetRangeMin Sets the minimum logical position for the slider in a trackbar.
+//	min Minimum position for the slider.
+//
+//	Remarks
+//	If the current slider position is less than the new minimum,
+//	the TBM_SETRANGEMIN message sets the slider position to the new minimum value.
+func (tb *TrackBar) SetRangeMin(redraw bool, min int) {
+	rd := 0
+	if redraw {
+		rd = 1
+	}
+	tb.SendMessage(winapi.TBM_SETRANGEMIN, uintptr(rd), uintptr(min))
+}
+
+//	GetSelEnd retrieves the ending position of the current selection range in a trackbar.
+//	Returns a 32-bit value that specifies the ending position of the current selection range.
+//	A trackbar can have a selection range only if you specified the TBS_ENABLESELRANGE style when you created it.
+func (tb *TrackBar) GetSelEnd() int {
+	return int(tb.SendMessage(winapi.TBM_GETSELEND, 0, 0))
+}
+
+//	GetSelStart retrieves the starting position of the current selection range in a trackbar.
+//	Returns a 32-bit value that specifies the starting position of the current selection range.
+//	A trackbar can have a selection range only if you specified the TBS_ENABLESELRANGE style when you created it.
+func (tb *TrackBar) GetSelStart() int {
+	return int(tb.SendMessage(winapi.TBM_GETSELSTART, 0, 0))
+}
+
+//	SetSel Sets the starting and ending positions for the available selection range in a trackbar.
+func (tb *TrackBar) SetSel(redraw bool, start, end uint16) {
+	rd := 0
+	if redraw {
+		rd = 1
+	}
+	tb.SendMessage(winapi.TBM_SETSEL, uintptr(rd), uintptr(win.MAKELONG(start, end)))
+}
+
+//	GetThumbLength retrieves the length of the slider in a trackbar.
+//	Returns the length, in pixels, of the slider.
+func (tb *TrackBar) GetThumbLength() int {
+	return int(tb.SendMessage(winapi.TBM_GETTHUMBLENGTH, 0, 0))
+}
+
+//	GetThumbRect retrieves the size and position of the bounding rectangle for the slider in a trackbar.
+func (tb *TrackBar) GetThumbRect() win.RECT {
+	var rect win.RECT
+	tb.SendMessage(winapi.TBM_GETTHUMBRECT, 0, uintptr(unsafe.Pointer(&rect)))
+	return rect
+}
+
+//	GetTooltips retrieves the handle to the tooltip control assigned to the trackbar, if any.
+//	Returns the handle to the tooltip control assigned to the trackbar, or NULL if tooltips are not in use.
+//	If the trackbar control does not use the TBS_TOOLTIPS style, the return value is NULL.
+func (tb *TrackBar) GetTooltips() win.HWND {
+	return win.HWND(tb.SendMessage(winapi.TBM_GETTOOLTIPS, 0, 0))
 }
 
 // NewTrackBar create a new TrackBar,need bind to Dialog before use.
